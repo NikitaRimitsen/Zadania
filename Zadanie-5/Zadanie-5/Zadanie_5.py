630927742c923a70db1e8db794b73b4335c362a3
NikitaRimitsen <77281903+NikitaRimitsen@users.noreply.github.com>
2021-05-06 11:14:46 +0300

    Add files via upload

---
+++ Zadanie-5/Zadanie-5/Zadanie_5.py
@@ -0,0 +1,9 @@
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+if b<=a>=c:
+    print("large number", a)
+elif a<=b>=c:
+    print("large number", b)
+else:
+    print("large number", c)
