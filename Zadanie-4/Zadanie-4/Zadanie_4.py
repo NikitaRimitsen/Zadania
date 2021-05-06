9629d15fbff593be6c75313756cd71671507e2b0
NikitaRimitsen <77281903+NikitaRimitsen@users.noreply.github.com>
2021-05-06 11:19:59 +0300

    Add files via upload

---
+++ Zadanie-4/Zadanie-4/Zadanie_4.py
@@ -0,0 +1,18 @@
+print("Large number")
+a=int(input("a="))
+b=int(input("b="))
+if a<b:
+    print("Large number", b)
+else:
+    print("Large number", a)
+
+
+print("--------------------------")
+
+print("Small number")
+a=int(input("a="))
+b=int(input("b="))
+if a>b:
+    print("Small number", b)
+else:
+    print("Small number", a)
